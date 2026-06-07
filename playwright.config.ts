288343bd4aba5cf8973d478d80528fa94e08e788
Tanmay Dandapat <tanmaydandapat@192.168.1.3>
2026-06-07 14:19:05 +0530

    config updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,64 +29,28 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    browserName: 'chromium',
     headless: false,
     screenshot: 'only-on-failure',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'retain-on-failure'
   },
 
-  /* Configure projects for major browsers */
-  projects: [
+   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],
-        browserName: 'chromium',
-        headless: false,
-        screenshot: 'only-on-failure',
-        trace : 'on-first-retry',
-       },
-    
+      use: { ...devices['Desktop Chrome'] }
     },
-
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'],
-        browserName: 'firefox',
-        headless: false,
-        screenshot: 'only-on-failure',
-        trace : 'on-first-retry',
-
-       },
+      use: { ...devices['Desktop Firefox'] }
     },
-
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-*/
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
+      use: { ...devices['Desktop Safari'] }
+    }
+  ]
+  /* Configure projects for major browsers. See https://playwright.dev/docs/api/class-testoptions. */
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
